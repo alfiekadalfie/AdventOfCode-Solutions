65e91d932908592ecd313c2737b91ff109c0e4a9
alfiekadalfie <75491385+alfiekadalfie@users.noreply.github.com>
2023-12-14 16:35:42 -0500

    Update day4-2023.py

--- day4-2023.py
+++ day4-2023.py
@@ -1,62 +1,64 @@
 sum = 0
-count = 0
+count = 0   # number of winning numbers
 winning_numbers = []
 
-''' # Part 1
+# Part 1
 for line in open("day4-2023.txt", "r"):
-    #print(line.strip("\n").split(": "))
-
+    # take only winning numbers and given numbers
     line = line.strip("\n").split(": ")[1].split(" | ")
 
+    # append each winning number
     for i in line[0].split(" "):
-        if(i is not ""):
+        if(i != ""):
             winning_numbers.append(i)
-    #print(winning_numbers)
-    
+    # increment count for each winning number found in set
     for j in winning_numbers:
         if(j in line[1].split(" ")):
             count+=1
-    print(count)
-    
+    # if there is at least one winning number, update sum by doubling for every match
     if(count > 0):
         sum += 2 ** (count - 1)
     
+    # reset
     count = 0
     winning_numbers = []
 
-print(sum)
-'''
+print("Part 1 Answer: " + str(sum))
 
 # Part 2
 card = 0    # card number
 
 original_cards = []
 
+# Each has 1 original card
 for k in range(216):
     original_cards.append(1)
 
 for line in open("day4-2023.txt", "r"):
+    # take only winning numbers and given numbers
     line = line.strip("\n").split(": ")[1].split(" | ")
 
+    # append each winning number
     for i in line[0].split(" "):
         if(i != ""):
             winning_numbers.append(i)
-    #print(winning_numbers)
     
+    # increment count for each winning number found in set
     for j in winning_numbers:
         if(j in line[1].split(" ")):
             count+=1
-    #print(count, card)
     
+    # update next 'count' cards
     for l in range(card+1, card+count+1):
         original_cards[l]+=original_cards[card]
     
+    # reset/update
     card+=1
     count = 0
     winning_numbers = []
 
+# add em all up
 for m in original_cards:
     sum += m
 
-print(original_cards)
-print(sum)
+print("Part 2 Answer: " + str(sum))
