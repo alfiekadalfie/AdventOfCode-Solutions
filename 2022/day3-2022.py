8ad8f8125a628f07e2db36461784427dd8be5d88
alfiekadalfie <75491385+alfiekadalfie@users.noreply.github.com>
2023-12-16 00:31:06 -0500

    Create day3-2022.py

---
+++ 2022/day3-2022.py
@@ -0,0 +1,41 @@
+dict = {"a":1, "b":2, "c":3, "d":4, "e":5, "f":6, "g":7, "h":8, "i":9, "j":10, "k":11, "l":12, "m":13, "n":14, "o":15, "p":16, "q":17, "r":18, "s":19, "t":20, "u":21, "v":22, "w":23, "x":24, "y":25, "z":26,
+        "A":27, "B":28, "C":29, "D":30, "E":31, "F":32, "G":33, "H":34, "I":35, "J":36, "K":37, "L":38, "M":39, "N":40, "O":41, "P":42, "Q":43, "R":44, "S":45, "T":46, "U":47, "V":48, "W":49, "X":50, "Y":51, "Z":52}
+
+# Part 1
+sum = 0
+
+for line in open("day3-2022.txt", "r"):
+    line = line.strip("\n")
+    split_rucksack = []
+    split_rucksack.append(line[0:len(line)//2])
+    split_rucksack.append(line[len(line)//2:])
+
+    for i in split_rucksack[0]:
+        if i in split_rucksack[1]:
+            sum += dict[i]
+            break
+
+print("Part 1 Answer: " + str(sum))
+
+# Part 2
+sum = 0
+threes = []
+count = 0
+
+for line in open("day3-2022.txt", "r"):
+    line = line.strip("\n")
+    threes.append(line)
+
+    count += 1
+
+    if count == 3:
+        print(threes)
+        for i in threes[0]:
+            if (i in threes[1]) and (i in threes[2]):
+                sum += dict[i]
+                break
+        
+        threes = []
+        count = 0
+
+print("Part 2 Answer: " + str(sum))
